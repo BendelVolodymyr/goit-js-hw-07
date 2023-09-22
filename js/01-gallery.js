966c81a14633d38bf5155dc4b3e6066882e800cd
BendelVolodymyr <bendel.vova@gmail.com>
2023-09-22 14:30:20 +0200

    finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,68 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+// 
+
+const gallery = document.querySelector('.gallery');
+
+
+const newImages = [];
+
+const newFotoGallery = (object) => {
+    object.map(({ preview, original, description }) => {
+        newImages.push(`<li class="gallery__item"><a class="gallery__link" href="${original}" ><img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}"/></a></li>`)
+    })
+    gallery.insertAdjacentHTML('beforeend', newImages.join(''));
+};
+
+newFotoGallery(galleryItems);
+
+// const clickKeyboard = ({ key }) => {
+//     if (key !== 'Escape') return;
+    
+//     console.log(key);
+// };
+
+// const clickImage = (event) => {
+//     event.preventDefault();
+//     var teg = event.target.tagName.toLowerCase();
+        
+//     if (teg !== 'img') return;
+//     basicLightbox.create(`<img src="${event.target.dataset.source}" width="800" height="600">`).show();
+//     document.addEventListener('keydown', clickKeyboard);
+    
+// };
+
+// function test() {
+//     basicLightbox.create().hiden();
+    
+// }
+
+
+// gallery.addEventListener('click', clickImage);
+
+
+function galleryModal(event){
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') return;
+
+  var instance = basicLightbox.create(`<img src="${event.target.dataset.source}">`, {
+    onShow: () => { window.addEventListener("keydown", onEsc) }, // опція-функція basicLightbox, додаємо подію при появі модалки, для кнопки 
+    onClose: () => { window.removeEventListener("keydown", onEsc) }// опція-функція basicLightbox, прибираємо подію після натискання Escape
+  });
+  // функція перевірки чи дійсно натиснула кнопка, повертає закриття модалки
+  function onEsc(event) {
+     if (event.code === 'Escape') {
+      instance.close();//
+    }
+  }
+  instance.show();
+};
+
+gallery.addEventListener("click", galleryModal);
+
+// console.log();
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+
+const newImages = [];
+
+const newFotoGallery = (object) => {
+    object.map(({ preview, original, description }) => {
+        newImages.push(`<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" /></a></li>`)
+    })
+    gallery.insertAdjacentHTML('beforeend', newImages.join(''));
+};
+
+newFotoGallery(galleryItems);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    sourceAttr: 'href', // завантаження файла з ...
+    overlayOpacity: 0.4, // прозорість фону
+    animationSpeed: 500, // Анімація перемикання слайдів (швидкість)
+    captionsData: 'alt', // додаємо з опису
+    captionPosition: 'bottom', // позиція
+    captionDelay: 250, // затримка опису '250 ms'
+});
+
+
+
+
